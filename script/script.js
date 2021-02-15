c700bf18f8d94cc4aa9ced90e8870d0845200f69
Yuri Gianlorenzi <yurigian@gmail.com>
2021-02-15 13:10:40 +0100

    add starting fliles

---
+++ script/script.js
@@ -0,0 +1,27 @@
+
+let app = new Vue({
+  el : '#app',
+  data : {
+
+  },//fine data
+
+  mounted() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+  },//fine mounted
+
+  methods : {
+
+  }//fine methods
+});//fine vue
