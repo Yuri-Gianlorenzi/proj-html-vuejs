c6a014d6dc5b9d0e08173fc7e03fbb898cc5bad1
Yuri Gianlorenzi <yurigian@gmail.com>
2021-02-17 14:25:51 +0100

    add sticky nav with vue

--- js/script.js
+++ js/script.js
@@ -2,26 +2,17 @@
 let app = new Vue({
   el : '#app',
   data : {
-
+    scrollPosition: null
   },//fine data
 
   mounted() {
-
-
-
-
-
-
-
-
-
-
-
-
-
+    window.addEventListener('scroll', this.updateScroll);
   },//fine mounted
 
   methods : {
-
+    updateScroll () {
+      this.scrollPosition = window.scrollY;
+      console.log(this.scrollPosition);
+    }
   }//fine methods
 });//fine vue
